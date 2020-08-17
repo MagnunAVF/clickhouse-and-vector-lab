d31cb215e386478cb2f2a4a4e7a5da294a27a7fa
Magnun A V F <magnunavf@gmail.com>
2020-08-17 12:29:54 -0300

    vector-clickhouse-env: Create nginx with lua container

---
+++ vector-clickhouse-env/nginx-openresty/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# prerequisites
+yum -y update
+yum install -y sudo curl
+
+mkdir -p /var/log/nginx
+# sudo ln -sf /dev/stderr /var/log/nginx/error.log
+# sudo ln -sf /dev/stdout /var/log/nginx/access.log
+
+# Setup vector
+curl -O https://packages.timber.io/vector/0.10.X/vector-x86_64.rpm
+
+sudo rpm -i vector-x86_64.rpm
+
+# Config vector
+cat <<-VECTORCFG > vector.toml
+[sources.in]
+  include = ["/var/log/nginx/access.log"] # required
+  type = "file" # required
+
+[sinks.out]
+  address = "vector_server:9000" # required
+  inputs = ["in"] # required
+  type = "vector" # required
+VECTORCFG

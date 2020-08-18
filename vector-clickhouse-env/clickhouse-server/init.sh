116e9f2ffb83d28dee7c5b0c2906ee0ad48c4da4
Magnun A V F <magnunavf@gmail.com>
2020-08-18 09:31:21 -0300

    vector-clickhouse-env: Set correct fields to clickhouse nginx_metrics table

--- vector-clickhouse-env/clickhouse-server/init.sh
+++ vector-clickhouse-env/clickhouse-server/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Create logs
-clickhouse-client --query "CREATE TABLE default.nginx_metrics (RemoteAddr String, TimeIso DateTime, Date Date DEFAULT toDate(TimeIso), RequestMethod String, RequestUri String, Status Int32, HttpUserAgent String) ENGINE = MergeTree(Date, (Status, Date), 8192)"
+clickhouse-client --query "CREATE TABLE default.nginx_metrics (host String, http_user_agent String, remote_addr String, request_method String, request_uri String, status Int32, timestamp DateTime, Date Date DEFAULT toDate(timestamp)) ENGINE = MergeTree(Date, (status, Date), 8192)"
